63ca79e9e44dba3c78043610671602fc9ab07040
LuizGandra <luizc.gandra29@gmail.com>
2025-07-01 19:23:26 -0300

    style: fix code style issues

--- include/ft_calloc.h
+++ include/ft_calloc.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcosta-g <lcosta-g@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/01 18:39:56 by lcosta-g          #+#    #+#             */
-/*   Updated: 2025/07/01 18:39:56 by lcosta-g         ###   ########.fr       */
+/*   Created: 2025/07/01 19:22:56 by lcosta-g          #+#    #+#             */
+/*   Updated: 2025/07/01 19:22:59 by lcosta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@
 
 typedef struct s_heap
 {
-	void *list[MAX_SIZE];
-	int i;
-} t_heap;
+	void	*list[MAX_SIZE];
+	int		i;
+}			t_heap;
 
-void	*ft_calloc(size_t size);
-void	ft_clear_heap(void);
+void		*ft_calloc(size_t size);
+void		ft_clear_heap(void);
 
 #endif
